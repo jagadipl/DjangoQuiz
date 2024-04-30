b0fedf00b54ced880fdbb328b8307e80967ea201
Jagadip Lamsal <jagadipl@yahoo.com>
2024-04-29 20:28:18 -0400

    Quiz App Completed

--- quizapp/views.py
+++ quizapp/views.py
@@ -4,9 +4,9 @@ from .models import Question
 def homepage(request):
     questions = Question.objects.all()
     if request.method == "POST":
-        score =0
+        score = 0
         for question in questions:
-            selected_option = request.POST[(str(question.id))]
+            selected_option = request.POST.get(str(question.id))
             if selected_option == question.correct_answer:
                 score+=1
         return render(request, 'quizapp/results.html', {
@@ -19,3 +19,6 @@ def homepage(request):
 
 def index(request):
     return render(request,'quizapp/index.html')
+
+def result(request):
+    return render(request,'quizapp/result.html')
